f3f1a85f48b00a2077e6c24081b1baecf1476f2d
joice-2008 <jesperaficosipp@gmail.com>
2026-06-25 15:21:16 -0300

    adicionando o banco

---
+++ cookie_hub.sql
@@ -0,0 +1,198 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 25/06/2026 às 20:19
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `cookie_hub`
+--
+CREATE DATABASE IF NOT EXISTS `cookie_hub` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `cookie_hub`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `api`
+--
+
+CREATE TABLE `api` (
+  `idIngrediente` int(11) NOT NULL,
+  `nome` text NOT NULL,
+  `calorias` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `receita`
+--
+
+CREATE TABLE `receita` (
+  `idReceita` int(11) NOT NULL,
+  `legenda` text NOT NULL,
+  `imagem` text NOT NULL,
+  `tags` text NOT NULL,
+  `data` date NOT NULL,
+  `idUsuario` int(11) NOT NULL,
+  `titulo` text NOT NULL,
+  `ingredientes` text NOT NULL,
+  `infosNutricionais` text NOT NULL,
+  `quantidadeIngredientes` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `salvo`
+--
+
+CREATE TABLE `salvo` (
+  `idSalvo` int(11) NOT NULL,
+  `idUsuario` int(11) NOT NULL,
+  `idReceita` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `tags`
+--
+
+CREATE TABLE `tags` (
+  `idTag` int(11) NOT NULL,
+  `nome` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `tags`
+--
+
+INSERT INTO `tags` (`idTag`, `nome`) VALUES
+(1, 'Doce'),
+(2, 'Salgado'),
+(3, 'Quente'),
+(4, 'Frio'),
+(5, 'Rápido'),
+(6, 'Demorado'),
+(7, 'Fitness'),
+(8, 'Calórico');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `idUsuario` int(11) NOT NULL,
+  `login` text NOT NULL,
+  `senha` text NOT NULL,
+  `nomeCompleto` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `api`
+--
+ALTER TABLE `api`
+  ADD PRIMARY KEY (`idIngrediente`);
+
+--
+-- Índices de tabela `receita`
+--
+ALTER TABLE `receita`
+  ADD PRIMARY KEY (`idReceita`),
+  ADD KEY `idUsuario` (`idUsuario`);
+
+--
+-- Índices de tabela `salvo`
+--
+ALTER TABLE `salvo`
+  ADD PRIMARY KEY (`idSalvo`),
+  ADD KEY `idUsuario` (`idUsuario`),
+  ADD KEY `idReceita` (`idReceita`);
+
+--
+-- Índices de tabela `tags`
+--
+ALTER TABLE `tags`
+  ADD PRIMARY KEY (`idTag`);
+
+--
+-- Índices de tabela `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`idUsuario`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `api`
+--
+ALTER TABLE `api`
+  MODIFY `idIngrediente` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `receita`
+--
+ALTER TABLE `receita`
+  MODIFY `idReceita` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `salvo`
+--
+ALTER TABLE `salvo`
+  MODIFY `idSalvo` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `tags`
+--
+ALTER TABLE `tags`
+  MODIFY `idTag` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT de tabela `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `receita`
+--
+ALTER TABLE `receita`
+  ADD CONSTRAINT `receita_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`idUsuario`);
+
+--
+-- Restrições para tabelas `salvo`
+--
+ALTER TABLE `salvo`
+  ADD CONSTRAINT `salvo_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`idUsuario`),
+  ADD CONSTRAINT `salvo_ibfk_2` FOREIGN KEY (`idReceita`) REFERENCES `receita` (`idReceita`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
