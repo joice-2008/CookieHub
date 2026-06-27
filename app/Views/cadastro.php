8bc2177b37f5904fb4dba02769f8a7fd8e18c65c
joice-2008 <jesperaficosipp@gmail.com>
2026-06-27 16:33:24 -0300

    criação dos campos de login e cadastro

--- app/Views/cadastro.php
+++ app/Views/cadastro.php
@@ -5,9 +5,36 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <title>CookieHub</title>
 </head>
 <body>
+    <div class="container">
+        <div>
+            <p class="titulo">Crie o seu cadastro</p>
+        </div>
+        <form action="" method="post">
+        <div class="mb-3">
+            <label for="nome" class="form-label">Nome Completo</label>
+            <input type="text" class="form-control" id="nome">
+        </div>
+        <div class="mb-3">
+            <label for="usuario" class="form-label">Usuario</label>
+            <input type="text" class="form-control" id="usuario">
+        </div>
+        <div class="mb-3">
+            <label for="senha" class="form-label">Senha</label>
+            <input type="password" class="form-control" id="senha">
+        </div>
+        <div>
+            <a href="<?= base_url() ?>">Cancelar</a>
+        </div>
+        <div>
+            <input type="submit" value="Cadastrar">
+        </div>
+
+        </form>
+    </div>
     
 </body>
 </html>
--- app/Views/login.php
+++ app/Views/login.php
@@ -1,11 +1,33 @@
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <title>CookieHub</title>
 </head>
 <body>
+    <div class="container">
+        <div>
+            <p class="titulo">Faça login no CookieHub</p>
+        </div>
+        <form action="" method="post">
+        <div class="mb-3">
+            <label for="usuario" class="form-label">Usuario</label>
+            <input type="text" class="form-control" id="usuario">
+        </div>
+        <div class="mb-3">
+            <label for="senha" class="form-label">Senha</label>
+            <input type="password" class="form-control" id="senha">
+        </div>
+        <div>
+            <input type="submit" value="Entrar">
+        </div>
+
+        </form>
+    </div>
     
 </body>
 </html>
