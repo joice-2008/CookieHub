2c7586d8d7fe4c3345fa98a7033be978df6af095
joice-2008 <jesperaficosipp@gmail.com>
2026-08-01 17:16:32 -0300

    atualização do menu

--- app/Views/receita/cadastrar.php
+++ app/Views/receita/cadastrar.php
@@ -1,260 +1,276 @@
-<h1>Cadastrar Receita</h1>
 
-<form action="<?= base_url('receita/salvar') ?>" method="post" enctype="multipart/form-data">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?= $this->include('menu') ?>
 
-    <div>
-        <label>Título</label><br>
-        <input type="text" name="titulo" required>
-    </div>
 
-    <br>
+    <h1>Cadastrar Receita</h1>
 
-    <div>
-        <label>Imagem</label><br>
-        <input type="file" name="imagem" accept="image/*">
-    </div>
+    <form action="<?= base_url('receita/salvar') ?>" method="post" enctype="multipart/form-data">
 
-    <br>
+        <div>
+            <label>Título</label><br>
+            <input type="text" name="titulo" required>
+        </div>
 
-    <div>
-        <label>Pesquisar ingrediente</label><br>
-        <input type="text" id="pesquisaIngrediente">
-        <button type="button"   id="btnPesquisar">>Pesquisar</button>
-    </div>
+        <br>
 
-    <br>
+        <div>
+            <label>Imagem</label><br>
+            <input type="file" name="imagem" accept="image/*">
+        </div>
 
-    <div id="resultadoPesquisa">
-        <!-- Resultados da API aparecerão aqui -->
-    </div>
+        <br>
+
+        <div>
+            <label>Pesquisar ingrediente</label><br>
+            <input type="text" id="pesquisaIngrediente">
+            <button type="button"   id="btnPesquisar">>Pesquisar</button>
+        </div>
+
+        <br>
+
+        <div id="resultadoPesquisa">
+            <!-- Resultados da API aparecerão aqui -->
+        </div>
+
+        <hr>
+
+        <h3>Ingredientes adicionados</h3>
+
+        <label>Ingrediente</label>
+
+    <label>Pesquisar ingrediente</label>
+
+    <input
+        type="text"
+        id="pesquisaIngrediente">
+
+    <button
+        type="button"
+        id="btnPesquisar">
+
+    Pesquisar
+
+    </button>
+
+    <br><br>
+
+    <div id="resultadoPesquisa"></div>
 
     <hr>
 
-    <h3>Ingredientes adicionados</h3>
+    <p id="ingredienteSelecionadoTexto">
+    Nenhum ingrediente selecionado.
+    </p>
 
-    <label>Ingrediente</label>
+    <label>Quantidade (g)</label>
 
-<label>Pesquisar ingrediente</label>
+    <input
+        type="number"
+        id="quantidade">
 
-<input
-    type="text"
-    id="pesquisaIngrediente">
+    <button
+        type="button"
+        id="btnAdicionar">
 
-<button
-    type="button"
-    id="btnPesquisar">
+    Adicionar
 
-Pesquisar
-
-</button>
-
-<br><br>
-
-<div id="resultadoPesquisa"></div>
-
-<hr>
-
-<p id="ingredienteSelecionadoTexto">
-Nenhum ingrediente selecionado.
-</p>
-
-<label>Quantidade (g)</label>
-
-<input
-    type="number"
-    id="quantidade">
-
-<button
-    type="button"
-    id="btnAdicionar">
-
-Adicionar
-
-</button>
-
-<br><br>
-
-
-
-</button>
-
-    <table border="1" width="100%" id="tabelaIngredientes">
-        <thead>
-            <tr>
-                <th>Ingrediente</th>
-                <th>Quantidade (g)</th>
-                <th>Ação</th>
-            </tr>
-        </thead>
-
-        <tbody id="listaIngredientes">
-
-        </tbody>
-    </table>
-
-    <div id="inputsOcultos"></div>
-
-    <br>
-
-    <label>Modo de preparo</label><br>
-
-    <textarea
-        name="legenda"
-        rows="8"
-        cols="70"
-        placeholder="Descreva o modo de preparo..."></textarea>
+    </button>
 
     <br><br>
-<h3>Tags</h3>
 
-<?php foreach ($tags as $tag): ?>
 
-    <label>
-        <input
-            type="checkbox"
-            name="tags[]"
-            value="<?= $tag['idTag']; ?>">
 
-        <?= esc($tag['nome']); ?>
-    </label>
+    </button>
+
+        <table border="1" width="100%" id="tabelaIngredientes">
+            <thead>
+                <tr>
+                    <th>Ingrediente</th>
+                    <th>Quantidade (g)</th>
+                    <th>Ação</th>
+                </tr>
+            </thead>
+
+            <tbody id="listaIngredientes">
+
+            </tbody>
+        </table>
+
+        <div id="inputsOcultos"></div>
+
+        <br>
+
+        <label>Modo de preparo</label><br>
+
+        <textarea
+            name="legenda"
+            rows="8"
+            cols="70"
+            placeholder="Descreva o modo de preparo..."></textarea>
+
+        <br><br>
+    <h3>Tags</h3>
+
+    <?php foreach ($tags as $tag): ?>
+
+        <label>
+            <input
+                type="checkbox"
+                name="tags[]"
+                value="<?= $tag['idTag']; ?>">
+
+            <?= esc($tag['nome']); ?>
+        </label>
+
+        <br>
+
+    <?php endforeach; ?>
 
     <br>
 
-<?php endforeach; ?>
+    <button type="submit">
+        Salvar Receita
+    </button>
 
-<br>
+    </form>
 
-<button type="submit">
-    Salvar Receita
-</button>
+    <script>
 
-</form>
+    const btnAdicionar = document.getElementById("btnAdicionar");
 
-<script>
+    const btnPesquisar = document.getElementById("btnPesquisar");
 
-const btnAdicionar = document.getElementById("btnAdicionar");
+    btnPesquisar.addEventListener(
+        "click",
+        pesquisarIngrediente
+    );
 
-const btnPesquisar = document.getElementById("btnPesquisar");
+    btnAdicionar.addEventListener("click", adicionarIngrediente);
+    let ingredientes = [];
 
-btnPesquisar.addEventListener(
-    "click",
-    pesquisarIngrediente
-);
-
-btnAdicionar.addEventListener("click", adicionarIngrediente);
-let ingredientes = [];
-
-async function pesquisarIngrediente(){
-    const pesquisa = document
-    .getElementById("pesquisaIngrediente")
-    .value;
-
-}
-function adicionarIngrediente(){
-
-    const ingrediente =
-        document.getElementById("ingrediente").value;
-
-    const quantidade =
-        document.getElementById("quantidade").value;
-
-    if(ingrediente == "" || quantidade == ""){
-
-        alert("Preencha os dois campos.");
-
-        return;
+    async function pesquisarIngrediente(){
+        const pesquisa = document
+        .getElementById("pesquisaIngrediente")
+        .value;
 
     }
+    function adicionarIngrediente(){
 
-   
-    const inputs = document.getElementById("inputsOcultos");
+        const ingrediente =
+            document.getElementById("ingrediente").value;
 
-ingredientes.push({
+        const quantidade =
+            document.getElementById("quantidade").value;
 
-    idApi: ingredienteSelecionado.id,
+        if(ingrediente == "" || quantidade == ""){
 
-    nome: ingredienteSelecionado.nome,
+            alert("Preencha os dois campos.");
 
-    imagem: ingredienteSelecionado.imagem,
+            return;
 
-    quantidade: quantidade,
+        }
 
-    calorias: ingredienteSelecionado.calorias,
+    
+        const inputs = document.getElementById("inputsOcultos");
 
-    carboidratos: ingredienteSelecionado.carboidratos,
+    ingredientes.push({
 
-    proteinas: ingredienteSelecionado.proteinas,
+        idApi: ingredienteSelecionado.id,
 
-    gorduras: ingredienteSelecionado.gorduras
+        nome: ingredienteSelecionado.nome,
 
-});
+        imagem: ingredienteSelecionado.imagem,
 
-atualizarTela();
+        quantidade: quantidade,
 
-document.getElementById("ingrediente").value = "";
+        calorias: ingredienteSelecionado.calorias,
 
-document.getElementById("quantidade").value = "";
+        carboidratos: ingredienteSelecionado.carboidratos,
 
-}
+        proteinas: ingredienteSelecionado.proteinas,
 
-function atualizarTela(){
-
-    const tabela = document.getElementById("listaIngredientes");
-    const inputs = document.getElementById("inputsOcultos");
-
-    tabela.innerHTML = "";
-    inputs.innerHTML = "";
-
-    ingredientes.forEach(function(item, indice){
-
-        tabela.innerHTML += `
-
-            <tr>
-
-                <td>${item.nome}</td>
-
-                <td>${item.quantidade} g</td>
-
-                <td>
-
-                    <button
-                        type="button"
-                        onclick="removerIngrediente(${indice})">
-
-                        Remover
-
-                    </button>
-
-                </td>
-
-            </tr>
-
-        `;
-
-        inputs.innerHTML += `
-
-            <input
-                type="hidden"
-                name="ingredientes[]"
-                value="${item.nome}">
-
-            <input
-                type="hidden"
-                name="quantidades[]"
-                value="${item.quantidade}">
-
-        `;
+        gorduras: ingredienteSelecionado.gorduras
 
     });
 
-}
-
-
-function removerIngrediente(indice){
-
-    ingredientes.splice(indice,1);
-
     atualizarTela();
 
-}
-</script>
+    document.getElementById("ingrediente").value = "";
+
+    document.getElementById("quantidade").value = "";
+
+    }
+
+    function atualizarTela(){
+
+        const tabela = document.getElementById("listaIngredientes");
+        const inputs = document.getElementById("inputsOcultos");
+
+        tabela.innerHTML = "";
+        inputs.innerHTML = "";
+
+        ingredientes.forEach(function(item, indice){
+
+            tabela.innerHTML += `
+
+                <tr>
+
+                    <td>${item.nome}</td>
+
+                    <td>${item.quantidade} g</td>
+
+                    <td>
+
+                        <button
+                            type="button"
+                            onclick="removerIngrediente(${indice})">
+
+                            Remover
+
+                        </button>
+
+                    </td>
+
+                </tr>
+
+            `;
+
+            inputs.innerHTML += `
+
+                <input
+                    type="hidden"
+                    name="ingredientes[]"
+                    value="${item.nome}">
+
+                <input
+                    type="hidden"
+                    name="quantidades[]"
+                    value="${item.quantidade}">
+
+            `;
+
+        });
+
+    }
+
+
+    function removerIngrediente(indice){
+
+        ingredientes.splice(indice,1);
+
+        atualizarTela();
+
+    }
+    </script>
+</body>
+</html>
+
+
